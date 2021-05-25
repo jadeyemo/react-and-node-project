1e74e04c6e3a80539807b8c0f1aae8a4456f02dd
Josiah Adeyemo <jadeyemo@korpeatech.com>
2021-05-25 17:01:39 +0100

    Can now post user data to server api

--- app/src/App.js
+++ app/src/App.js
@@ -1,54 +1,95 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Button from './components/Button';
+import Persons from './components/Persons';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
+      id: '',
+      first_name: '',
+      last_name: '',
     };
   }
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  componentDidMount() {
-    fetch("http://192.168.33.10:5000/getjson")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { id, first_name, last_name } = this.state;
+
+    const person = {
+      id,
+      first_name,
+      last_name,
+    };
+
+    axios
+      .post('http://192.168.31.1:5000/create', person)
+      .then(() => console.log('Person Added!'))
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.first_name}>
-              {item.first_name} {item.last_name}
-            </li>
-          ))}
-        </ul>
-      );
-    }
+    return(
+      <Router>
+        <div>
+        <br />
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <div style={{ width: '30%' }} className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="id"
+                placeholder="ID"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <br />
+            <div style={{ width: '30%' }} className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="first_name"
+                placeholder="First Name"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <br />
+            <div style={{ width: '30%' }} className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="last_name"
+                placeholder="Last Name"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <br />
+            <div style={{ width: '30%' }}>
+              <button className="btn btn-success" type="submit">
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+          <Link to={'/persons'} className="nav-link"> <Button color='blue' text='View persons' /> </Link>
+          <Switch>
+              <Route path='/persons' component={Persons} />
+          </Switch>
+        </div>
+        </Router>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const data = require('./data/data.json');
+const logger = require('morgan');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      }
+));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(function(req, res, next){
-    console.log(`${req.method} requrest for ${req.url}`);
+    console.log(`${req.method} request for ${req.url}`);
     next();
 });
 
@@ -15,8 +25,24 @@ app.get('/getjson', function(req, res){
     res.json(data);
 });
 
+app.post('/create', function(req, res) {
+    const newPerson = {
+      id: req.body.id,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+    };
+  
+    data.push(newPerson);
+    let json = JSON.stringify(data);
+    fs.writeFile('./data/data.json', json, (err) => {
+        if (err) throw err;
+        console.log(json);
+    } );
+
+  });
+  
 
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), () => {
     console.log(`Server is running on port ${app.get('port')}`);
 });
